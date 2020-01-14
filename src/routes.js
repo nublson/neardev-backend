213a8b39a539db8acc2d8169382ecb667ac81a62
nubelsondev <nubelsondev@hotmail.com>
2020-01-14 15:27:53 +0000

    fix(DevController): index method for list all devs

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const routes = require('express').Router()
 
 const DevController = require('./controllers/DevController')
 
+routes.get('/devs', DevController.index)
 routes.post('/devs', DevController.store)
 
 module.exports = routes
