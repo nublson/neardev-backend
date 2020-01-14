c887b5566325f3cc2b3e66a821d494412bb66ae9
nubelsondev <nubelsondev@hotmail.com>
2020-01-14 13:39:10 +0000

    feat: db connection

--- src/server.js
+++ src/server.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv/config')
 
 const routes = require('./routes')
+const dbUrl = process.env.MONGO_URL
+const port = process.env.PORT || 3333
 
 const app = express()
+mongoose.connect(dbUrl, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
 
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => console.log('Running...'))
+app.listen(port, () => console.log(`Port ${port} is running...`))
