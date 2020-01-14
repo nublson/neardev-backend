88e12c639bd0b1377a1d604811b8258ba7e995b4
nubelsondev <nubelsondev@hotmail.com>
2020-01-14 15:21:43 +0000

    fix(DevController): store method for dev login

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,5 +1,32 @@
+const axios = require('axios')
+
+const Dev = require('../models/Dev')
+
 module.exports = {
 	async store(req, res) {
-		return res.json(req.body)
+		const { github_username, techs } = req.body
+
+		let dev = await Dev.findOne({ github_username })
+
+		if (!dev) {
+			const techsArray = techs
+				.split(',')
+				.map(tech => tech.trim().toUpperCase())
+
+			const response = await axios.get(
+				`https://api.github.com/users/${github_username}`
+			)
+			const { name = login, avatar_url, bio } = response.data
+
+			dev = await Dev.create({
+				name,
+				github_username,
+				bio,
+				techs: techsArray,
+				avatar_url
+			})
+		}
+
+		return res.json(dev)
 	}
 }
