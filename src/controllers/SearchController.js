61397afe6feefece80966ba59be08b84def92c5c
nubelsondev <nubelsondev@hotmail.com>
2020-01-14 16:37:57 +0000

    fix(SearchControler): search devs by tech and proximity

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -5,10 +5,25 @@ const parseStringAsArray = require('../utils/parseStringAsArray')
 module.exports = {
 	async index(req, res) {
 		const { techs, latitude, longitude } = req.query
-		return res.json({
-			techs,
-			latitude,
-			longitude
+		const techsArray = parseStringAsArray(techs)
+
+		const devs = await Dev.find({
+			//Filter by techs
+			techs: {
+				$in: techsArray
+			},
+			// Search all devs within 10 kilometers (10000 meters)
+			location: {
+				$near: {
+					$geometry: {
+						type: 'Point',
+						coordinates: [longitude, latitude]
+					},
+					$maxDistance: 10000
+				}
+			}
 		})
+
+		return res.json(devs)
 	}
 }
